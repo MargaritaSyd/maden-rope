24f5fa481e7509a2890bb58665a5222a23b2ac65
MargaritaSyd <margarita.syddall@gmail.com>
2021-12-04 18:13:42 -0300

    seteando localStorage favoritos

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,5 +1,5 @@
 window.addEventListener("load" , function(){
-
+ //localStorage.clear()
 
     let fav = document.getElementById("fav");
         fav.style.display  = "none"
@@ -10,6 +10,51 @@ window.addEventListener("load" , function(){
      notFav.addEventListener("click" , function(){
      notFav.style.display = "none"
      fav.style.display = "inline"
+/*
+     let favInfo = {
+        favId: window.location.pathname.slice(8),
+        favName: document.querySelector("#productName").innerHTML,
+        favDescription: document.querySelector("#productDescription").innerHTML,
+        favImg: document.querySelector("#detail-img").src,
+        favPrice: document.querySelector("#productPrice").innerHTML,
+          
+        }
+       localStorage.setItem("productFav" , JSON.stringify(favInfo))
+
+       let favList = [];
+       if(localStorage.getItem("favList") == null){
+
+        if(localStorage.getItem("productFav") != null){
+            
+            let parseProductFav = JSON.parse(localStorage.getItem("productFav"));
+    
+            localStorage.removeItem('productFav')
+
+    
+            favList.push(parseProductFav)
+
+            localStorage.setItem("favList" , JSON.stringify(favList))
+            
+            }
+        
+       } else {
+
+        if(localStorage.getItem("favList") != null){
+        let parseProductFav = JSON.parse(localStorage.getItem("productFav"));
+
+        let parseFavList = JSON.parse(localStorage.getItem("favList"));
+
+        localStorage.removeItem('productFav')
+
+       // localStorage.removeItem('favList')
+
+        parseFavList.push(parseProductFav)
+
+        localStorage.setItem("favList" , JSON.stringify(parseCartList))
+
+       }
+       }
+  */          
      })
     
 
