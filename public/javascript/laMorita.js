3665334d14d61260a02ab7f17def337e728eedba
MargaritaSyd <margarita.syddall@gmail.com>
2021-12-19 23:02:25 -0300

    changes

---
+++ public/javascript/laMorita.js
@@ -0,0 +1,13 @@
+let card1 = document.getElementById("card1");
+
+let divImg1 = document.getElementById("divImg1");
+
+divImg1.addEventListener("mouseover" , function(){
+    divImg1.style.display = "none"
+    card1.style.display = "inline"
+})
+
+card1.addEventListener("mouseover" , function(){
+    card1.style.display = "none"
+    divImg1.style.display = "inline"
+})
